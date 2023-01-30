69caa22b8e34be382dee80abdf5be0b7311d349a
Dmytrii Furs <furs.dmitry@gmail.com>
2023-01-30 15:00:52 -0500

    Initialization

---
+++ APIMe/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using APIMe.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace APIMe.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ APIMe/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace APIMe.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
