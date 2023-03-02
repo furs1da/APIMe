baec754cd19542a41ff744faa12e8b4b1d37177c
Dmytrii Furs <furs.dmitry@gmail.com>
2023-03-01 23:29:14 -0500

    Working launching

--- APIMe/Program.cs
+++ APIMe/Program.cs
@@ -1,110 +1,73 @@
-using APIMe.Data;
 using APIMe.Models;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
-using APIMe.Utilities.EmailSender;
-using Microsoft.AspNetCore.Hosting;
 using APIMe.Tokens;
-using Microsoft.AspNetCore.Builder;
-using APIMeContext = APIMe.Models.APIMeContext;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Hosting;
 using APIMe.Interfaces;
 using APIMe.Services;
+using APIMe.Utilities.EmailSender;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
-
-// Add services to the container.
-
-
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-
+// Configure services
 builder.Services.Configure<MailSettings>(options =>
 {
     builder.Configuration.GetSection("MailSettings").Bind(options);
 });
-
-
-builder.Services.AddTransient<IMailService, MailService>();
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddMemoryCache();
 builder.Services.AddSession();
-
-
-var connectionString = builder.Configuration.GetConnectionString("APIMeConnection");
-builder.Services.AddDbContext<APIMe.Models.APIMeContext>(options =>
-    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(
-    options =>
-    {
-        options.Password.RequiredLength = 6;
-        options.Password.RequireNonAlphanumeric = true;
-        options.Password.RequireDigit = true;
-        options.User.RequireUniqueEmail = true;
-        options.SignIn.RequireConfirmedEmail = true;
-        options.Tokens.EmailConfirmationTokenProvider = "emailconfirmation";
+builder.Services.AddDbContext<APIMeContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireDigit = true;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = true;
+    options.Tokens.EmailConfirmationTokenProvider = "EmailConfirmation";
 
-        options.Lockout.AllowedForNewUsers = true;
-        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
-        options.Lockout.MaxFailedAccessAttempts = 3;
-    }
-)
-.AddEntityFrameworkStores<APIMe.Models.APIMeContext>()
-.AddDefaultTokenProviders()
-.AddTokenProvider<EmailConfirmationTokenProvider<User>>("emailconfirmation");
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2);
+    options.Lockout.MaxFailedAccessAttempts = 3;
+})
+    .AddEntityFrameworkStores<APIMeContext>()
+    .AddDefaultTokenProviders()
+    .AddTokenProvider<EmailConfirmationTokenProvider<IdentityUser>>("EmailConfirmation");
 
 builder.Services.Configure<DataProtectionTokenProviderOptions>(options => options.TokenLifespan = TimeSpan.FromHours(2));
 builder.Services.Configure<EmailConfirmationTokenProviderOptions>(options => options.TokenLifespan = TimeSpan.FromDays(3));
-
-builder.Services.AddIdentityServer()
-    .AddApiAuthorization<ApplicationUser, APIMe.Data.APIMeContext>();
-
-builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
-
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddTransient<IMailService, MailService>();
 
-
+// Configure the app
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthentication();
-app.UseIdentityServer();
 app.UseAuthorization();
 
-app.UseSession();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-app.MapRazorPages();
-
+app.MapControllers();
 app.MapFallbackToFile("index.html");
 
-await APIMeContext.CreateAdminUserAsync(app.Services.GetRequiredService<IServiceProvider>());
+// Run the app
 app.Run();
 
--- APIMe/Tokens/EmailConfirmationTokenProvider.cs
+++ APIMe/Tokens/EmailConfirmationTokenProvider.cs
@@ -1,18 +1,40 @@
 ﻿using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
 
 namespace APIMe.Tokens
 {
-    public class EmailConfirmationTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    public class EmailConfirmationTokenProvider<TUser> : DataProtectorTokenProvider<TUser>
+        where TUser : class
     {
         public EmailConfirmationTokenProvider(IDataProtectionProvider dataProtectionProvider,
-            IOptions<EmailConfirmationTokenProviderOptions> options,
+            IOptions<DataProtectionTokenProviderOptions> options,
             ILogger<DataProtectorTokenProvider<TUser>> logger)
             : base(dataProtectionProvider, options, logger)
         {
         }
+
+        public override async Task<string> GenerateAsync(string purpose,
+            UserManager<TUser> manager, TUser user)
+        {
+            return await base.GenerateAsync(purpose, manager, user);
+        }
+
+        public override async Task<bool> ValidateAsync(string purpose, string token,
+            UserManager<TUser> manager, TUser user)
+        {
+            return await base.ValidateAsync(purpose, token, manager, user);
+        }
+
+        public override async Task<bool> CanGenerateTwoFactorTokenAsync(
+            UserManager<TUser> manager, TUser user)
+        {
+            return false;
+        }
     }
+
     public class EmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
     {
     }
