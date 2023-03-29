6a9a9b2cd6b3df4f86a7b4910cb4a5ac36f33fd0
Canna0329 <47868758+Canna0329@users.noreply.github.com>
2023-03-29 19:48:52 -0400

    Having trouble with jwthandler
    Cannot figure out how it works

--- APIMeTests/Controllers/AccountControllerTests.cs
+++ APIMeTests/Controllers/AccountControllerTests.cs
@@ -14,6 +14,15 @@ using Microsoft.Extensions.Configuration;
 using Moq;
 using System.Security.Claims;
 using APIMe.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using APIMe.Services.Email;
+using APIMe.Entities.DataTransferObjects.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using NSubstitute;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Castle.Core.Configuration;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace APIMe.Controllers.Tests
 {
@@ -26,28 +35,42 @@ namespace APIMe.Controllers.Tests
         {
             IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
             IConfiguration configuration = configurationBuilder.Build();
+
             var mockStore = new Mock<IUserStore<IdentityUser>>();
-            var userManager = new UserManager<IdentityUser>(mockStore.Object, null, null, null, null, null, null, null, null);
-            JwtHandler jwtHandler = new JwtHandler(configuration, userManager);
+
+            var userManager = new Mock<UserManager<IdentityUser>>(mockStore.Object, null, null, null, null, null, null, null, null);
+            userManager.Setup(p => p.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(new IdentityUser { Email="test", EmailConfirmed =true, Id="4", UserName ="test" });
+            userManager.Setup(p => p.IsEmailConfirmedAsync(It.IsAny<IdentityUser>())).ReturnsAsync(true);
+            userManager.Setup(p => p.CheckPasswordAsync(It.IsAny<IdentityUser>(),It.IsAny<string>())).ReturnsAsync(true);
+
+            JwtHandler jwtHandler = new JwtHandler(configuration,userManager.Object);
             APIMeContext ContextDataAccess = new MockContext<APIMeContext>().GetMockContext();
 
-            /*controller = new AccountController(userManager, ContextDataAccess, jwtHandler);
+            var email = new Mock<IEmailSender>();
+            controller = new AccountController(userManager.Object, ContextDataAccess, jwtHandler, email.Object);
             var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim(ClaimTypes.Name, "testingmockup"),
             }, "mock"));
-            controller.ControllerContext.HttpContext = new DefaultHttpContext() { User = user };*/
+            controller.ControllerContext.HttpContext = new DefaultHttpContext() { User = user };
         }
         [TestMethod()]
-        public void SectionListTest()
+        public async Task SectionListTest()
         {
-            Assert.Fail();
+            IActionResult result = await controller.SectionList();
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, actual: ((OkObjectResult)result).StatusCode);
         }
 
         [TestMethod()]
-        public void LoginTest()
+        public async Task LoginTest()
         {
-            Assert.Fail();
+            UserForAuthenticationDto user=new UserForAuthenticationDto();
+            user.Email = "test";
+            user.Password = "test";
+            IActionResult result = await controller.Login(user);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, actual: ((OkObjectResult)result).StatusCode);
         }
 
         [TestMethod()]
--- APIMeTests/MockContext.cs
+++ APIMeTests/MockContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using APIMe.Entities.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace APIMeTests
 {
@@ -27,8 +28,9 @@ namespace APIMeTests
 
                 // Sections => table name
                 context.Sections.Add(new Section { Id = 4, SectionName = "n/a", ProfessorId = 0, AccessCode = "n/a" });
-                context.UserClaims.Add(new Microsoft.AspNetCore.Identity.IdentityUserClaim<string> { ClaimType="", ClaimValue="", Id=0, UserId="0"});
+                context.UserClaims.Add(new IdentityUserClaim<string> { ClaimType="", ClaimValue="", Id=0, UserId="0"});
                 context.Professors.Add(new Professor { Id = 0, Email = "testingmockup", FirstName = "first", LastName = "last" });
+                context.Users.Add(new IdentityUser { Email ="test", EmailConfirmed= true, Id="0"  });
 
                 context.SaveChanges();
                 return context;
